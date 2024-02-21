f1f29d6009e46625d887eeb09bc757556e20955d
amukoaterry <amukoaterrymitchell@gmail>
2024-02-21 23:58:49 +0300

    add files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,38 @@
+fun main(){
+    printFullName("Terry", "Mitchell")
+    printFullName("Jack", "Black")
+    printFullName("Yoweri", "Museveni")
+    printFullName("Jacinta", "Mwende")
+
+}
+    fun add(num1: Int, num2:Int){
+        var sum = num1+num2
+        println(sum)
+    }
+
+fun getStringLength(word: String){
+    var word = "Lake Tanganyika"
+    var len = word.length
+    println(len)
+}
+
+fun printFullName(firstName: String, lastName: String){
+    var fullName = firstName + " " +lastName
+    println(fullName)
+
+    }
+
+//write a function that takes in 2 numbers
+//and prints out the result of their accurate division
+//eg given 5 and 2 it should print out 2.5
+
+fun divide(){
+    var num1 =5
+    var num2 =2
+    var result =(5/2)
+    println(result)
+}
+
+
+
+
